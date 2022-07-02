ad51f0eb72696bf85267ccd2a12c2f6e7a33ae48
Mikkychi <michealchima08@gmail.com>
2022-07-02 08:46:45 -0700

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  * - Micheal Chima, 2022-07-02",
  *   followed by a new line, to the standard error.
  *   Return: 1 if success.
- *   */
+ */
 int main(void)
 {
 	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
