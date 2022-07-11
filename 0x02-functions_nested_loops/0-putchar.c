ba117275644fa75b735c663bf3bc28b390d1a14a
Mikkychi <michealchima08@gmail.com>
2022-07-11 06:56:26 -0700

    pitchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,17 @@
 #include "main.h"
+
 /**
- * main - Entry point
- *
- * Return: always 0(Success)
+ * main - prints a string of characters
+ * Description:prints _putchar, followed by a new line
+ * Return: 0
  */
-int main(word)
+int main(void)
 {
-	char str[] = "_putchar";
-	int ch;
-
-	for (ch =0; ch < 8; ++ch)
-		_putchar(str[ch]);
-	_putchar('\n');
-
-	return (0);
+	char a [] = "_putchar";
+	int i;
+	for (i=0; i < 8; i++)
+		_putchar(a[i]);
+}
+_putchar('\n');
+return (0);
 }
